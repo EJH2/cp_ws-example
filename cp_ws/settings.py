4af27ef2f632f2b65a923fe24121146b5e7ba1da
Ethan <ethan.harriz@gmail.com>
2020-10-03 23:58:33 -0400

    Move Django secret key to .env

--- cp_ws/settings.py
+++ cp_ws/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y!mltxqyyqcj8z-xkv^e=j_%e+2dz3eh2n+n8%2()0f^#8^7=p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
